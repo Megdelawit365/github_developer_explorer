358ddb84a7c63e28c83a6d0ed6fb27c816b32906
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-08 19:06:21 +0300

    added function for fetching repository data from github api

--- js/githubApi.js
+++ js/githubApi.js
@@ -27,3 +27,35 @@ const fetchUserData = async (username) => {
         }
     }
 }
+
+
+
+const fetchRepositoryData = async (username) => {
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}/repos?per_page=100&sort=updated`)
+        if (!response.ok) {
+            let message = ""
+            switch (response.status) {
+                case 404:
+                    message = "Github user not found."
+                    break
+                case 403 || 429:
+                    message = "GitHub API rate limit reached. Please stop making unnecessary retries."
+                    break
+                default:
+                    message = "Unknown Github API error."
+            }
+            throw new Error(message)
+        }
+        const data = await response.json()
+        return {
+            success: true,
+            data: data
+        }
+    } catch (error) {
+        return {
+            success: false,
+            error: error.message
+        }
+    }
+}

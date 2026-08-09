71f8b1cc2e8fba099e3262ef364da98db17de7b7
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-09 18:53:58 +0300

    fixed date calculation error and added profile avatar

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,7 @@
 const profileError = document.querySelector('.profile-error')
 const profileInfo = document.querySelector('.profile-info')
+const profilePic = document.querySelector('.profile-pic')
+
 const displayProfileError = (error) => {
     profileError.textContent = error
 }
@@ -16,21 +18,24 @@ const displayProfileInfo = (profile) => {
     const createdAt = document.createElement('p')
     const accountAge = document.createElement('p')
 
-    const date = new Date(profile.createdAt)
+    const date = new Date(profile.created_at)
     const today = new Date()
     const formattedDate = date.toLocaleDateString('en-US', { month: 'long', day: "numeric", year: "numeric" })
-    const timeDiff = Math.floor((today.getTime() - date.getTime()) / (1000 * 60 * 60 * 24))
+    const yearDiff = Math.floor((today.getTime() - date.getTime()) / (1000 * 60 * 60 * 24 * 365))
 
     name.textContent = `NAME: ${profile.name || "Not provided."}`
     bio.textContent = `BIO: ${profile.bio || "Not provided."}`
     company.textContent = `COMPANY: ${profile.company || "Not provided."}`
     location.textContent = `LOCATION: ${profile.location || "Not provided."}`
-    followers.textContent = `FOLLOWERS: ${profile.followers || "Not provided."}`
-    following.textContent = `FOLLOWING: ${profile.following || "Not provided."}`
-    publicRepo.textContent = `PUBLIC REPOSITORIES: ${profile.public_repo || "Not provided."}`
-    profileLink.textContent = `PROFILE LINK: ${profile.url || "Not provided."}`
-    createdAt.textContent = `CREATED AT: ${formattedDate || "Not provided."}`
-    accountAge.textContent = `ACCOUNT AGE: ${timeDiff} days.`
+    followers.textContent = `FOLLOWERS: ${profile.followers}`
+    following.textContent = `FOLLOWING: ${profile.following}`
+    publicRepo.textContent = `PUBLIC REPOSITORIES: ${profile.public_repos}`
+    profileLink.textContent = `PROFILE LINK: ${profile.url}`
+    createdAt.textContent = `CREATED AT: ${formattedDate}`
+    accountAge.textContent = `ACCOUNT AGE: ${yearDiff} years.`
+
+    profilePic.style.backgroundImage = url(`${avatar_url}`)
+    profilePic.style.display = "block"
 
     profileInfo.append(name, bio, company, location, followers, following, publicRepo, profileLink, createdAt, accountAge)
 }

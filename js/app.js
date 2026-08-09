9f3ec9f037e9e11bb984bf6d40bca5831a5b65d6
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-09 18:09:14 +0300

    added event handler for search  button

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const searchButton = document.querySelector('.search-btn')
+const searchInput = document.querySelector('.search-input')
+
+import { fetchRepositoryData } from "./githubApi.js"
+import { fetchUserData } from "./githubApi.js"
+import { displayProfileError, displayProfileInfo } from "./ui.js"
+
+searchButton.addEventListener('click', async () => {
+    const username = searchInput.value
+    if (!username) {
+        displayProfileError("Enter a username first.")
+        return
+    }
+    const [profile, repository] = await Promise.all([
+        fetchUserData(username),
+        fetchRepositoryData(username)
+    ])
+    if (!profile.success) {
+        displayProfileError(profile.error)
+        return
+    }
+    console.log(profile)
+    displayProfileInfo(profile.data)
+
+
+})

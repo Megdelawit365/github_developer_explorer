a5a78ee9a0608a85f635881a9d0bb85834843cb7
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-09 13:55:32 +0300

    removed the duplicated logic

--- js/githubApi.js
+++ js/githubApi.js
@@ -1,6 +1,6 @@
-const fetchUserData = async (username) => {
+const fetchData = async (url) => {
     try {
-        const response = await fetch(`https://api.github.com/users/${username}`)
+        const response = await fetch(url)
         if (!response.ok) {
             let message = ""
             switch (response.status) {
@@ -28,34 +28,12 @@ const fetchUserData = async (username) => {
     }
 }
 
+const fetchUserData = async (username) => {
+    fetchData(`https://api.github.com/users/${username}`)
+}
 
 
 const fetchRepositoryData = async (username) => {
-    try {
-        const response = await fetch(`https://api.github.com/users/${username}/repos?per_page=100&sort=updated`)
-        if (!response.ok) {
-            let message = ""
-            switch (response.status) {
-                case 404:
-                    message = "Github user not found."
-                    break
-                case 403 || 429:
-                    message = "GitHub API rate limit reached. Please stop making unnecessary retries."
-                    break
-                default:
-                    message = "Unknown Github API error."
-            }
-            throw new Error(message)
-        }
-        const data = await response.json()
-        return {
-            success: true,
-            data: data
-        }
-    } catch (error) {
-        return {
-            success: false,
-            error: error.message
-        }
-    }
+    fetchData(`https://api.github.com/users/${username}/repos?per_page=100&sort=updated`)
 }
+
